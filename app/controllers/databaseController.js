88e2c625819d8e53e82202a21ec38f85490f0045
Swapnil Gupta <sgupta133@sapient.com>
2016-12-01 18:00:26 +0530

    Updated for separation concerns.

--- app/controllers/databaseController.js
+++ app/controllers/databaseController.js
@@ -8,5 +8,37 @@ module.exports = {
             });    
         })
         return allUsersPrmoise;
+    },
+    findAndEditUser: function(req){
+        findAndEditUserPromise = new Promise(function(resolve, reject){
+            User.findOne({ 'local.email' :  req.body.email }, function(err, user) {
+                // if there are any errors, return the error before anything else
+                if (err)
+                    console.log(err);
+
+                // if no user is found, return the message
+                /*if (!user){
+                    console.log(user);
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                }*/
+
+                // if the user is found but the password is wrong
+                /*if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata*/
+                if(user){
+                    user.local.name = req.body.name;
+                    user.save(function(err) {
+                        if(!err) {
+                            resolve(user);
+                        }
+                        else {
+                            console.log("Error: could not save contact " + user.local.name);
+                        }
+                    });
+                }
+
+            });
+        });
+        return findAndEditUserPromise;
     }
 }
--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,36 @@
 // app/routes.js
 var User = require('../app/models/user');
 var databaseController = require('../app/controllers/databaseController');
+var authenticationController = require('../app/controllers/authenticationController');
 module.exports = function(app, passport) {
-
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+    //Home page listing all the users
     app.get('/', function(req, res) {
         var allUsersPrmoise = databaseController.findAllUsers();
         allUsersPrmoise.then(function(result){
             res.render('index.ejs', {
                 users : result // get the user out of session and pass to template
-            }); // load the index.ejs file
+            });
         })
         .catch(function(reason){
             console.log('Handle rejected promise ('+reason+') here.');
         });
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
+    //Login page
     app.get('/login', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') }); 
     });
-    /*app.get('/edit', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('edit.ejs', { message: req.flash('loginMessage') }); 
-    });*/
-    // process the login form
-    // process the login form
+    
+    //Login form Authentication
     app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
+    
+    //Signup page
     app.get('/signup', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
@@ -55,75 +41,40 @@ module.exports = function(app, passport) {
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-    app.get('/edit', isLoggedIn, function(req, res) {
-        console.log("edit");
-        console.log(req.user);
-       res.render('edit.ejs', {
+    
+    //Show Edit page after checking whether the user is logged in or not
+    app.get('/edit', authenticationController.isLoggedIn, function(req, res) {
+        res.render('edit.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
-    app.post('/edit', isLoggedIn, function(req, res) {
-        //console.log(req.body.name);
-        //console.log(req.body.email);
-        User.findOne({ 'local.email' :  req.body.email }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            if (err)
-                console.log(err);
-
-            // if no user is found, return the message
-            if (!user)
-                console.log(user);
-                //return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
-            // if the user is found but the password is wrong
-            /*if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata*/
-            if(user){
-                user.local.name = req.body.name;
-                user.save(function(err) {
-                    if(!err) {
-                        console.log(user);
-                        res.render('profile.ejs', {
-                            user : user,
-                            profileUpdate: true// get the user out of session and pass to template
-                        });
-                    }
-                    else {
-                        console.log("Error: could not save contact " + user.local.name);
-                    }
-                });
-            }
-            
+    
+    //Update the user after editing the details
+    app.post('/edit', authenticationController.isLoggedIn, function(req, res) {
+        var findAndEditUserPromise = databaseController.findAndEditUser(req);
+        findAndEditUserPromise.then(function(user){
+            res.render('profile.ejs', {
+                user : user,
+                profileUpdate: true// get the user out of session and pass to template
+            });
+        })
+        .catch(function(reason){
+            console.log('Handle rejected promise ('+reason+') here.');
         });
+        
     });
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
+    
+    //User Profile Page
+    app.get('/profile', authenticationController.isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
     
-
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
+    //Logout the user when user clicks on Logout
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
 };
 
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
